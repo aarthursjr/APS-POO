f583007b52c165b7d5f43af82a3f2762c700e8a5
Filipe Duarte <filipegduarte@gmail.com>
2024-10-08 11:52:03 -0300

    add: funcionalidade de registro de avaliação dos alunos

--- programaBuscaResponsavel/programaBuscaResponsavel/src/main/java/buscaResponsavel/Aluno.java
+++ programaBuscaResponsavel/programaBuscaResponsavel/src/main/java/buscaResponsavel/Aluno.java
@@ -1,14 +1,38 @@
 package buscaResponsavel;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 public class Aluno {
     private String nome;
+    private Avaliacao avaliacao;
 
-    public Aluno(String nome) {
+    public Aluno(String nome, Avaliacao avaliacao) {
         this.nome = nome;
+        this.avaliacao = avaliacao;
     }
 
     public String getNome() {
         return nome;
+    }
+
+    public Avaliacao getAvaliacao() {
+        Avaliacao avaliacao = this.avaliacao;
+
+        if ( validaDataLimite(avaliacao.getData(), 2) == false ) {
+            this.avaliacao.setNota(-1);
+            this.avaliacao.setComentario("");
+        }
+
+        return this.avaliacao;
+    }
+
+    private boolean validaDataLimite(LocalDateTime data, Integer qtdDias) {
+        if ( data.plusDays(qtdDias).isAfter(LocalDateTime.now()) ) {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
     @Override
